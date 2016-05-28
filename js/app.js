fef01a5dacc57ff3a565e34588321d467c1fae6b
geofflightbourn <glightbourn80@gmail.com>
2016-05-28 00:54:44 -0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-/*
-  Please add all Javascript code to this file.
-*/
+function processHandlebarsTemplate(templateId, data) {
+	var templateSource = $('#' + templateId).html();
+	var compiledTemplate = Handlebars.compile(templateSource);
+
+	return compiledTemplate(data);
+}
+
+$.ajax({
+	type: 'GET',
+	url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
+	success: function(response) {
+    var templateSource = $('#newsTemp').html()
+    			var compiledTemplate = Handlebars.compile(templateSource)
+        response.data.feed.forEach(function (article) {
+          var data = article
+    			var generatedHtml = compiledTemplate(data)
+    			$('#main').append(generatedHtml)
+        })
+    }
+})
+
+$( "h3" ).click(function() {
+  console.log('clicked');
+  $('[href]:first-of-type').removeClass('.hidden')
+})
