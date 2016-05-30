304acb8b4e35d0a6be03e15d759ac90f1333cad6
geofflightbourn <glightbourn80@gmail.com>
2016-05-29 19:13:30 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -19,9 +19,25 @@ $.ajax({
     }
 })
 
+$.ajax({
+	type: 'GET',
+	url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
+	success: function(response) {
+    var templateSource = $('#popUpTemp').html()
+    			var compiledTemplate = Handlebars.compile(templateSource)
+        response.data.feed.forEach(function (article) {
+          var data = article
+    			var generatedHtml = compiledTemplate(data)
+    			$('#popUp').append(generatedHtml)
+        })
+    }
+})
+
 $(document).ready(function() {
-	$( ".contentHeader" ).click(function() {
-	  console.log('clicked');
-	  $('#popUp').removeClass('hidden')
-		})
+
+	$( "#main" ).on( "click", "a", function( event ) {
+		console.log('clicked');
+	$( this ).removeClass('hidden, loader')
+	});
+
   })
