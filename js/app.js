a8320f62cf56449792c5aa545c44437aeabb3ce4
geofflightbourn <glightbourn80@gmail.com>
2016-06-07 20:30:29 -0500

    pretty much done

--- js/app.js
+++ js/app.js
@@ -4,10 +4,6 @@ function processHandlebarsTemplate(templateId, data) {
 	return compiledTemplate(data);
 }
 
-$( document ).ajaxError(function( event, request, settings ) { //ajax error message
-  alert('error loading page')
-});
-
 $.ajax({ //Gets 10 digg articles
 	type: 'GET',
 	url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
@@ -15,39 +11,69 @@ $.ajax({ //Gets 10 digg articles
     var templateSource = $('#newsTemp').html()
     			var compiledTemplate = Handlebars.compile(templateSource)
 
-				window.data = response.data.feed
+					var articles = response.data.feed
 
-        response.data.feed.forEach(function (article, index) {
-          var data = article
-					data.index = index
+articles.forEach(function(article) {
+	var articleHtml = processHandlebarsTemplate('newsTemp', article)
+	var $articleContainer = $(articleHtml).appendTo('#main')
 
-    			var generatedHtml = compiledTemplate(data) //#main
-    			$('#main').append(generatedHtml)
-        })
+	$articleContainer.on('click', 'h3.title', function(event) {
+		console.log('You clicked on the following article:')
+		$('#popUp').removeClass('hidden')
+		$('#popUp').removeClass('loader')
 
-				var popUpTemplate = Handlebars.compile($('#popUpTemp').html()) //#popUp
-				$('#popUp').append(popUpTemplate({
-	 			data: response
- 			}));
-    }
-})
+		$('#popUp').append('<h1>"'+ article.content.title +'"</h1>')
+		$('#popUp').append('<p>"'+ article.content.description +'"<p>')
+		$('#popUp').append()
+	})
+		})
+     	}
+				}) //end Digg request
 
-var $loading = $('.loader').hide(); //loader circle
-$(document)
-.ajaxStart(function () {
-	$loading.show();
-})
-.ajaxStop(function () {
-	$loading.hide();
-	});
 
+				$.ajax({ //Gets 10 Reddit articles
+					type: 'GET',
+					url: 'https://accesscontrolalloworiginall.herokuapp.com/https://www.reddit.com/r/news.json',
+					success: function(response) {
+				    var templateSource = $('#redditTemp').html()
+				    			var compiledTemplate = Handlebars.compile(templateSource)
+
+									var articles = response.data.children
+
+				articles.forEach(function(article) {
+					var articleHtml = processHandlebarsTemplate('redditTemp', article)
+					var $articleContainer = $(articleHtml).appendTo('#main')
+
+					$articleContainer.on('click', 'h3.title', function(event) {
+						console.log('You clicked on the following article:')
+						$('#popUp').removeClass('hidden')
+						$('#popUp').removeClass('loader')
+
+						$('#popUp').append('<h1>"'+ article.data.title +'"</h1>')
+						$('#popUp').append('<p>"'+ article.content.domain +'"<p>')
+						$('#popUp').append()
+					})
+						})
+				     	}
+								}) //end Reddit request
+
+$( document ).ajaxError(function( event, request, settings ) { //ajax error message
+  alert('error loading page')
+});
 
 $(document).ready(function() {
 
+	var $loading = $('.loader').hide(); //loader circle
+	$(document).ajaxStart(function () {
+		$loading.show();
+	}).ajaxStop(function () {
+		$loading.hide();
+		});
+
 	$( "#main" ).on( "click", "a", function( event ) { //Load Popup
-		var index = $(this).data('article-index')
-		var article = window.data[index]
-		console.log(article);
+		// var index = $(this).data('article-index')
+		// var article = window.data[index]
+		// console.log(article);
 	$('#popUp').show()
 	$('.hidden').removeClass()
 	$('').appendTo('#popUp')
